4cd8002f4f8bcbaf67ff29b488e98e750e678ae2
sahithi <nagasahikuthuru231@gmail.com>
2026-09-16 16:04:35 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+    let n= numbers.length;
+    let left= 0;
+    let right= n-1;
+while(left<right){
+    let sum= numbers[left]+ numbers[right]
+    if(sum>target){
+        right--;
+    }
+     if(sum<target){
+        left++;
+    }
+    if(sum == target){
+        return [left+1,right+1]  
+    }
+
+    }
+    }}
